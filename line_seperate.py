7e9d8678bdfb0b6617d72d5321c4749dfb04bdad
rakeshvar <rakeshvar@gmail.com>
2015-02-03 01:15:29 -0800

    Fixed bug in detecting last line.

--- line_seperate.py
+++ line_seperate.py
@@ -5,6 +5,9 @@ from scipy import ndimage
 import numpy as np
 from PIL import Image as im
 
+def log(*args, **kwargs):
+    if False:
+        print(*args, **kwargs)
 
 class Image():
     def __init__(self, path):
@@ -18,22 +21,20 @@ class Image():
     def filter_noise(self,):
         self.data = ndimage.median_filter(self.data, 3)
 
-    def morph_all(self,):
-        self.closed = ndimage.binary_closing(self.data)
-        self.opened = ndimage.binary_opening(self.data)
-
     def calc_hist(self,):
-        self.hist = np.sum(self.data, axis=1).astype('float')
-        hist_mean = np.mean(self.hist)
-        self.fft = abs(np.fft.rfft(self.hist - hist_mean))
+        hist_ = np.sum(self.data, axis=1).astype('float')
+        hist_mean = np.mean(hist_)
+        self.fft = abs(np.fft.rfft(hist_ - hist_mean))
         max_harm = int(np.argmax(self.fft))
+        self.best_harmonic = self.ht // (1 + max_harm)
         assert max_harm > 0
 
-        self.best_harmonic = self.ht // (1 + max_harm)
+        self.closed = ndimage.binary_closing(self.data, structure=np.ones((1, self.best_harmonic//4)))
+        self.hist = np.sum(self.closed, axis=1).astype(float)
         self.gaus_hist = ndimage.filters.gaussian_filter1d(self.hist,
                     self.best_harmonic/16, mode='constant',
                     cval=0,
-                    # truncate=4.0,
+                    truncate=4.0,
         )
         self.d_gaus_hist = ndimage.filters.convolve(self.gaus_hist, [-1, 0, 1])
 
@@ -46,15 +47,17 @@ class Image():
         inpeak = False
         min_dist_in_peak = self.best_harmonic / 2.0
         self.base_lines = []
+        log("Max Hist: {:.2f} Peakthresh: {:.2f} Zerothresh: {:.2f}"
+              "Min Dist in Peak: {:.2f}".format(gmaxval, peakthresh, zerothresh, min_dist_in_peak))
 
         for i, val in enumerate(hist):
             if not inpeak:
                 if val > peakthresh:
-                    print('transition to in-peak @ ', i, val)
                     inpeak = True
                     maxval = val
                     maxloc = i
                     mintosearch = i + min_dist_in_peak
+                    log('\ntransition to in-peak: mintosearch : ', mintosearch, end='')
                     # accept no zeros between i and i+mintosearch
 
             else:  #  in peak, look for max
@@ -62,11 +65,18 @@ class Image():
                     maxval = val
                     maxloc = i
                     mintosearch = i + min_dist_in_peak
+                    log('\nMoved mintosearch to', mintosearch, end='')
                 elif i > mintosearch and val <= zerothresh:
                     # leave peak and save the last baseline found
                     inpeak = False
-                    print('Found baseline @ ', maxloc)
+                    log('\nFound baseline #', maxloc, end='')
                     self.base_lines.append(maxloc)
+
+            log(' @{}'.format(i), end='')
+
+        if inpeak:
+            self.base_lines.append(maxloc)
+            log('\nFound baseline #', maxloc, end='')
 
         self.num_lines = len(self.base_lines)
 
@@ -78,7 +88,7 @@ class Image():
             # Find top lines
             if i == 0:  frm = 0
             else:   frm = self.line_sep[i]
-            print(" Searching for top line in range : ", frm+1, base)
+            log(" Searching for top line in range : ", frm+1, base)
             top_at = np.argmin(self.d_gaus_hist[frm+1:base])
             self.top_lines.append(frm + 1 + top_at)
 
@@ -109,30 +119,22 @@ if __name__ == '__main__':
                           img.hist,
                           img.gaus_hist,
                           img.d_gaus_hist):
-        print(l, i, j, k)
-    print()
+        log("{:4d} {:7.2f} {:7.2f} {:7.2f}".format(l, i, j, k))
+    log()
 
     img.find_baselines()
     img.separate_lines()
 
     print('\nImage Properties:')
-    for k, v in img.__dict__.items():
+    for k in sorted(img.__dict__):
+        v = getattr(img, k)
         try:
             print(k, ':', v.shape, '(shape)')
         except AttributeError:
             print(k, ':', v)
     print()
 
-    for i in range(img.num_lines):
-        print(img.line_sep[i])
-        print(img.top_lines[i], img.base_lines[i])
-    print(img.line_sep[-1])
-
-    line = img.get_line(img.num_lines//2)
-    ht, wd = line.shape
-    line = line[ht//4:3*ht//4, wd//4:wd//4+80]
-    pprint(line)
-
+    ################################## Show Image with Lines
     img_col = img.img.convert("RGB")
     draw = id.Draw(img_col)
     def line(ats, col):
